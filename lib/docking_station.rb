ca9b7f033336ed0b171dd0d6f28d7f3c3d01e4ea
sunaliseth <sunaliseth01@gmail.com>
2017-08-31 16:39:57 +0100

    11. Added feature test to dock bike, unit test to see bike docked and attr reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,15 @@
 require_relative 'bike.rb'
 
 class DockingStation
+attr_reader :bike
 
   def release_bike
   #  "release bike"
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
   end
 
 end
