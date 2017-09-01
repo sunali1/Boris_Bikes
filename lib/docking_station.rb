30633c026ddf313004608b17c39b0f7f9d9513a6
benjaminsunderland <ben@sunderland.eu.com>
2017-09-01 08:05:37 -0700

    12. Raising Exceptions, 13. Limiting Capacity , 14.Using Complex Attributes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,23 @@
 require_relative 'bike.rb'
 
 class DockingStation
-attr_reader :bike
+
+ attr_reader :bike
+ attr_accessor :bikes
+
+ def initialize
+  @bikes = []
+  end
 
   def release_bike
   #  "release bike"
-    Bike.new
+    fail "No bikes available" unless @bikes.count >= 1
+    @bikes.pop
   end
 
   def dock(bike)
-    @bike = bike
+    fail "Bike Rack Full" if @bikes.count >= 20
+    @bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,16 +2,33 @@ require 'docking_station'
 
 describe DockingStation do
     it "releases working bikes" do
-      bike = subject.release_bike
+      bike = Bike.new
+      subject.dock(bike)
+      subject.release_bike
       expect(bike).to be_working
     end
+
     it "docks bikes" do
       bike = Bike.new
-      expect(subject.dock(bike)).to eq bike
+      expect(subject.dock(bike)).to eq [bike]
     end
+
     it "bike available" do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bike).to eq @bikes
     end
+
+    describe '#release_bike ' do
+    it 'No bikes available' do
+    expect { subject.release_bike }.to raise_error ("No bikes available")
+    end
+  end
+
+    it 'raises an error when full' do
+      bike = Bike.new
+      20.times { subject.dock(bike) }
+      expect { subject.dock(bike) }.to raise_error ("Bike Rack Full")
+    end
+
 end
